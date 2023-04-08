1154a3a824b66eeef92c7ea5bd12607ddda723d9
Mawan <maawan18@gmail.com>
2023-04-09 03:33:40 +0530

    feroifn

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express();
 app.get('/',(req,res)=>{
-    res.send("hello world")
+    res.sendFile(__dirname + "/temp.html")
 })
 app.listen(process.env.PORT | 3001)
